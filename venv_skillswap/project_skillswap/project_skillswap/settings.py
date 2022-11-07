7cb7c08be728b2641773e7271081319f7c665235
gomaiwasaki8 <goma81orange2002@icloud.com>
2022-11-07 09:54:20 +0900

    11/7 開発用ロギング設定完了　古越

--- venv_skillswap/project_skillswap/project_skillswap/settings.py
+++ venv_skillswap/project_skillswap/project_skillswap/settings.py
@@ -133,3 +133,44 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+#ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    #ロガーの設定
+    'loggers': {
+        #Djsngoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        #skillswapアプリケーションが利用するロガー
+        'skillswap': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    #ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    #フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
