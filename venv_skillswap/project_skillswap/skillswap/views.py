c7861976512c7c437f81d46da110d02f244f95e7
gomaiwasaki8 <goma81orange2002@icloud.com>
2023-01-12 16:52:13 +0900

    1/12授業終了

--- venv_skillswap/project_skillswap/skillswap/views.py
+++ venv_skillswap/project_skillswap/skillswap/views.py
@@ -464,7 +464,10 @@ def addFriend(request, username):
         current_user.user_friends.create(friend=friend)  # ログオンユーザ視点でフレンドを登録
         friend.user_friends.create(friend=current_user)  # フレンド視点でログオンユーザをフレンドに登録
         # フレンドになったら取引が許可されたことをrequestテーブルに保存
-        # request.request_completed.update(request_completed=True)
+        object = Request.objects.get(user_id_id=current_user, receiver_id_id=friend)
+        context = {'object': object}
+        object.request_completed = True
+        object.save()
     return redirect("skillswap:search_user")
 
 
